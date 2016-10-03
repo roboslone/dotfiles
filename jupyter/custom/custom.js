a12bc8b9244cfcb3758952bf6cc7bc4cbef18cb8
roboslone <roboslone@yandex-team.ru>
2016-10-03 12:45:20 +0300

    prettier jupyter notebook theme

--- jupyter/custom/custom.js
+++ jupyter/custom/custom.js
@@ -1,4 +1,5 @@
 $([IPython.events]).on("app_initialized.NotebookApp", function () {
     $('div#header-container').hide();
     $('div#maintoolbar').hide();
+    $('div#notebook-container').css({boxShadow: 'none', backgroundColor: '#eeeeee'})
 });

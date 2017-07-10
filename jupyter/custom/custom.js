43238971e9626f6cbb425df5eca124d7677e6eaa
roboslone <me@robosl.one>
2017-07-10 23:25:35 +0300

    removed jupyter custom files

--- jupyter/custom/custom.js
+++
@@ -1,6 +0,0 @@
-$([IPython.events]).on("app_initialized.NotebookApp", function () {
-    $('div#header-container').hide();
-    $('div#maintoolbar').hide();
-    $('div#notebook-container').css({boxShadow: 'none', backgroundColor: '#eeeeee'});
-    $('#menus').hide();
-});

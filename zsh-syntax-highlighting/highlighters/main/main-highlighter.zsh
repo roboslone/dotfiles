df0cfeb4367a138260ba09eff24347323603745f
roboslone <roboslone@yandex-team.ru>
2017-08-14 00:38:06 +0300

    highlighting updated

--- zsh-syntax-highlighting/highlighters/main/main-highlighter.zsh
+++ zsh-syntax-highlighting/highlighters/main/main-highlighter.zsh
@@ -32,24 +32,24 @@
 # Define default styles.
 : ${ZSH_HIGHLIGHT_STYLES[default]:=none}
 : ${ZSH_HIGHLIGHT_STYLES[unknown-token]:=fg=red,bold}
-: ${ZSH_HIGHLIGHT_STYLES[reserved-word]:=fg=yellow}
-: ${ZSH_HIGHLIGHT_STYLES[alias]:=fg=green}
-: ${ZSH_HIGHLIGHT_STYLES[builtin]:=fg=green,bold}
-: ${ZSH_HIGHLIGHT_STYLES[function]:=fg=green}
-: ${ZSH_HIGHLIGHT_STYLES[command]:=fg=green}
-: ${ZSH_HIGHLIGHT_STYLES[precommand]:=fg=green}
-: ${ZSH_HIGHLIGHT_STYLES[commandseparator]:=none}
-: ${ZSH_HIGHLIGHT_STYLES[hashed-command]:=fg=green}
-: ${ZSH_HIGHLIGHT_STYLES[path]:=underline}
+: ${ZSH_HIGHLIGHT_STYLES[reserved-word]:=fg=magenta}
+: ${ZSH_HIGHLIGHT_STYLES[alias]:=fg=white,bold}
+: ${ZSH_HIGHLIGHT_STYLES[builtin]:=fg=white,bold}
+: ${ZSH_HIGHLIGHT_STYLES[function]:=fg=white,bold}
+: ${ZSH_HIGHLIGHT_STYLES[command]:=fg=white,bold}
+: ${ZSH_HIGHLIGHT_STYLES[precommand]:=fg=white,bold}
+: ${ZSH_HIGHLIGHT_STYLES[commandseparator]:=fg=white,bold}
+: ${ZSH_HIGHLIGHT_STYLES[hashed-command]:=fg=white,bold}
+: ${ZSH_HIGHLIGHT_STYLES[path]:=underline,fg=blue}
 : ${ZSH_HIGHLIGHT_STYLES[globbing]:=fg=blue}
 : ${ZSH_HIGHLIGHT_STYLES[history-expansion]:=fg=blue}
-: ${ZSH_HIGHLIGHT_STYLES[single-hyphen-option]:=none}
-: ${ZSH_HIGHLIGHT_STYLES[double-hyphen-option]:=none}
-: ${ZSH_HIGHLIGHT_STYLES[back-quoted-argument]:=none}
-: ${ZSH_HIGHLIGHT_STYLES[single-quoted-argument]:=fg=yellow}
-: ${ZSH_HIGHLIGHT_STYLES[double-quoted-argument]:=fg=yellow}
-: ${ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]:=fg=cyan}
-: ${ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]:=fg=cyan}
+: ${ZSH_HIGHLIGHT_STYLES[single-hyphen-option]:=fg=cyan}
+: ${ZSH_HIGHLIGHT_STYLES[double-hyphen-option]:=fg=cyan}
+: ${ZSH_HIGHLIGHT_STYLES[back-quoted-argument]:=fg=cyan}
+: ${ZSH_HIGHLIGHT_STYLES[single-quoted-argument]:=fg=green}
+: ${ZSH_HIGHLIGHT_STYLES[double-quoted-argument]:=fg=green}
+: ${ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]:=fg=white}
+: ${ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]:=fg=white}
 : ${ZSH_HIGHLIGHT_STYLES[assign]:=none}
 
 # Whether the highlighter should be called or not.

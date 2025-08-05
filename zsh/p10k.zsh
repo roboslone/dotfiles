f48ded56a2f8d19bd87287685e509c55c752726d
Aleksandr Khristiukhin <ak@robosl.one>
2025-08-05 22:22:57 +0300

    fixed root template

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -95,7 +95,7 @@
 
   # Context format when root: user@host. The first part white, the rest grey.
   # typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE="%F{$white}%n%f%F{$grey}@%m%f"
-  typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE="%F{$blue}@%m%f"
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE="%F{$blue}%m%f"
   # Context format when not root: user@host. The whole thing grey.
   # typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE="%F{$grey}%n@%m%f"
   typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE="%F{$blue}%m%f"

0f96d2f4f26e9ed2cfdfbb77d1886cb2ce61cb79
Aleksandr Khristiukhin <ak@robosl.one>
2025-08-05 19:57:16 +0300

    way better completions

--- zsh/common.zsh
+++ zsh/common.zsh
@@ -23,6 +23,8 @@
     compinit
     setopt completealiases
     zstyle ':completion:*' menu select
+    zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+    zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
     unsetopt nomatch
 
 # shell history
